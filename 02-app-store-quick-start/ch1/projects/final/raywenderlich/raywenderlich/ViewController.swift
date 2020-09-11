ff33254029ac957f8dab81f0a76ba4fc06238734
Pietro Rea <pietro@sweetpeamobile.com>
2020-09-11 17:06:51 -0400

    Added more screenshot sizes & final project.

---
+++ 02-app-store-quick-start/ch1/projects/final/raywenderlich/raywenderlich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  raywenderlich
+//
+//  Created by Pietro Rea on 9/3/20.
+//  Copyright © 2020 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
